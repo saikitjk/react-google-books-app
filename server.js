16c93f733f291d63757fe18e076b01c4b7676ede
JK <saikitjk@gmail.com>
2020-11-16 09:54:56 -0800

    created function to handle save book

--- server.js
+++ server.js
@@ -15,20 +15,14 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
-app.use(routes);
+app.use("/api", routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
 
 mongoose.connect(
-  process.env.mongodb_connection_string,
+  process.env.MONGODB_URI ||
+    `mongodb+srv://${process.env.mongoDBConnectionURL}`,
   {
     useNewUrlParser: true,
     useFindAndModify: false,
@@ -40,3 +34,11 @@ mongoose.connect(
     console.log("MongoDB connection established.");
   }
 );
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
