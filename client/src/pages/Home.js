6dafd28f7e6cc45d9e4f4a37be57dd1dff9b4494
JK <saikitjk@gmail.com>
2020-11-15 22:19:21 -0800

    added check saved books and load book shelf and function for handle Input

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,6 +11,24 @@ class Home extends Component {
     searched: "",
   };
 
+  checkSaved = (id) => {
+    for (let i in this.state.bookShelf) {
+      if (this.state.bookShelf[i].id === id) return true;
+    }
+    return false;
+  };
+  loadBookShelf = () => {
+    API.getBookShelf().then((res) => {
+      this.setState({ bookShelf: res.data });
+    });
+  };
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
     return <div className="container booklist_container"></div>;
   }
