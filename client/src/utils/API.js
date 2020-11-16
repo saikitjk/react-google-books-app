6456703d0555563ec58b0754f1a2d4f481b3e6f2
JK <saikitjk@gmail.com>
2020-11-15 20:04:18 -0800

    added get, post delete methods for books saved in bookshelf

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,4 +4,16 @@ export default {
   getBooks: function (query) {
     return axios.get("/api/books", { params: { q: query } });
   },
+
+  getBookShelf: function () {
+    return axios.get("/api/bookshelf");
+  },
+
+  saveBookShelf: function (bookData) {
+    return axios.post("/api/bookshelf", bookData);
+  },
+
+  deleteBooks: function (id) {
+    return axios.delete(`/api/bookshelf/${id}`);
+  },
 };
